b3581fb2ce70fbe8eba68a9e6b4b72c546be97e1
sujan <sujan@sdfd.com>
2019-02-17 17:02:55 +0530

    com

---
+++ src/test/java/sandeep/ApplicationTestCase.java
@@ -0,0 +1,21 @@
+package sandeep;
+
+import junit.framework.Assert;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class ApplicationTestCase extends GWTTestCase
+{
+   private static final String MODULE_NAME = "sandeep.Application";
+
+   public String getModuleName() {
+      return MODULE_NAME;
+   }
+
+   public void testSomething() {
+      // Not much to actually test in this sample app
+      // Ideally you would test your Controller here (NOT YOUR UI)
+      // (Make calls to RPC services, test client side model objects, test client side logic, etc)
+      Assert.assertTrue( true );
+   }
+}
